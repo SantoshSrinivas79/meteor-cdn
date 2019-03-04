f3522d3c923738314ac879d49d386616633ca0b6
Mark Downey <mark@thebakery.io>
2019-03-04 15:50:16 +0000

    remove template helper and dependency

--- package.js
+++ package.js
@@ -1,26 +1,24 @@
 Package.describe({
   name: 'thebakery:cdn',
-  version: '1.4.0',
+  version: '1.4.2',
   summary: 'Serve Meteor content from a CDN',
   git: 'https://github.com/bakery/meteor-cdn',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4');
+  api.versionsFrom('1.4.2');
   api.export('CDN','server');
   api.export('CDN','client');
   api.use('webapp','server');
-  api.use('templating','client', {weak: true});
   api.use('browser-policy', {weak: true});
-  api.addFiles('lib/template.js','client');
   api.addFiles('lib/client.js','client');
   api.addFiles('lib/server.js','server');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('nitrolabs:cdn');
+  api.use('thebakery:cdn');
   api.addFiles('tests/client.js','client');
   api.addFiles('tests/server.js','server');
 
